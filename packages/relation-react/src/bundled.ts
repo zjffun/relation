c1dfff24119a9f4ab4bacec1343a6396d5f411fe
Jufeng Zhang <zjffun@gmail.com>
2023-01-23 23:06:43 +0800

    fix: type

--- packages/relation-react/src/bundled.ts
+++ packages/relation-react/src/bundled.ts
@@ -1,9 +1,9 @@
-export { getFileContentKey } from '../../relation-core/dist/core/getFileContentKey';
-export { relationId } from '../../relation-core/dist/core/relationId';
+export { getFileContentKey } from 'relation2-core/dist/core/getFileContentKey';
+export { relationId } from 'relation2-core/dist/core/relationId';
 
 export {
   IRelationViewerData,
   IViewerContents,
   IViewerRelation,
   PartTypeEnum,
-} from '../../relation-core/dist/types';
+} from 'relation2-core/dist/types';
